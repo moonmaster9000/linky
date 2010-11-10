1bc7f081b35f6117b651c40c069b38d8d20ae092
Matt Parker <moonmaster9000@gmail.com>
2010-11-10 13:54:55 -0500

    linky phase 1 working!

--- lib/linky/linky.rb
+++ lib/linky/linky.rb
@@ -1,22 +1,40 @@
 class Linky
   class << self
-    def link(keyword, html, &block)
+    def link(keyword, target, html, options={})
+      block = proc {|keyword| %{<a href="#{target}" #{options.map {|k,v| %{#{k}="#{v}"}}.join " "}>#{keyword}</a>}}
       html_fragment = Nokogiri::HTML::DocumentFragment.parse html
-      real_link keyword.to_s, haystack, &block
+      real_link keyword.to_s, html_fragment, &block
+      html_fragment.to_html
     end
 
     private
     def real_link(needle, haystack, &block)
+      # if we've drilled down all the way to a raw text node,
+      # and it contains the needle we're looking for
       if haystack.text? && (match = haystack.content.match(/(#{needle})/i))
+        
+        # call the block to determine what we'll replace the needle with
         new_needle = block.call match[0]
-        haystack.after new_needle
-        haystack.after haystack.content.gsub(/.*[\ .]#{match[0]}/, "")
-        haystack.content = haystack.content.gsub(/(.*)#{match[0]}/, "\1")
+       
+        # break the string into three parts
+        prefix, keyword, postfix = break_on_boundary match[0], haystack.to_s 
+        
+        haystack.content = prefix
+        haystack.add_next_sibling Nokogiri::HTML::DocumentFragment.parse(new_needle)
+        if !postfix.nil? and !postfix.empty?
+          haystack.next_sibling.add_next_sibling Nokogiri::HTML::DocumentFragment.parse(postfix)
+          haystack.next_sibling.next_sibling.content = postfix
+        end
       elsif haystack.name != "a" && haystack.respond_to?(:children)
         haystack.children.each do |child|
           real_link needle, child, &block 
         end
       end
+    end
+
+    def break_on_boundary(boundary, string)
+      match = string.match /^(.*)(#{boundary})(.*)$/
+      return match[1], match[2], match[3]
     end
   end
 end
--- spec/linky_spec.rb
+++ spec/linky_spec.rb
@@ -7,15 +7,15 @@ describe Linky, "#link" do
     @html = "<p>some html with keyword and Keyword and <a href='#'>keyword</a></p>"
   end
 
-  it "should require a needle, haystack, and block" do
+  it "should require a keyword, a target, some html, and potentially options" do
     proc {Linky.link}.should raise_error
     proc {Linky.link :keyword}.should raise_error
-    proc {Linky.link :keyword, @html}.should raise_error
-    proc {Linky.link :keyword, @html {|k| "<a href='http://linkhere!'>#{k}</a>"}}.should_not raise_error
+    proc {Linky.link :keyword, "http://link.to_somewhere"}.should raise_error
+    proc {Linky.link :keyword, "http://link.to.somewhere", @html, :class => "autolinked"}.should_not raise_error
   end
 
   it "should link any keywords not already linked" do
-    new_html = Linky.link @keyword, @html {|k| "<a href='http://link.to.somewhere'>#{k}</a>"}
-    new_html.should == "<p>some html with <a href='http://link.to.somewhere'>keyword</a> and <a href='http://link.to.somewhere'>Keyword</a> and <a href='#'>keyword</a></p>"
+    new_html = Linky.link(@keyword, "http://link.to.somewhere", @html)
+    new_html.should == "<p>some html with <a href=\"http://link.to.somewhere\">keyword</a> and Keyword and <a href=\"#\">keyword</a></p>"
   end
 end
